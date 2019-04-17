8d3b896811c307020b4330b9b509d8a360c86cb8
ccrawf7 <31708058+ccrawf7@users.noreply.github.com>
2019-04-16 23:55:11 -0400

    changes to browse page

--- metube_template/browse.php
+++ metube_template/browse.php
@@ -30,17 +30,30 @@ function saveDownload(id)
 </head>
 
 <?php
-//if(!isset($_SESSION['username'])){
-//	header('Refresh:0; index.php');
-//}
+	$query1 = "SELECT media.mediaid, media.filename, media.filepath, media.title, media.description FROM media ORDER BY mediaid DESC";
+    $bquery = $query1;
+    $title = "Browse";
 
+    if(isset($_POST['category']) && $_POST['category'] != ""){
+        $category = $_POST['category'];
+        $query2 = "SELECT media.mediaid, media.filename, media.filepath, media.title, media.description  FROM media WHERE category='$category' ORDER BY mediaid DESC";
+        $bquery = $query2;
+        $title = "Category: ".$category;
+    }
+    else if(isset($_POST['search']) && $_POST['search'] != ""){
+        $keyword=$_POST['search'];
+        $query3 = "SELECT media.mediaid, media.filename, media.filepath, media.title, media.description FROM `keywords` INNER JOIN media on keywords.mediaid = media.mediaid WHERE keyword='$keyword'  ORDER BY mediaid DESC";
+        $bquery = $query3;
+        $title = "Keyword Search on '".$keyword."'";
+    }
+
+    $bresult = mysql_query($bquery);
+	
 ?>
-<body>
-<p>Welcome <?php if(!isset($_SESSION["username"]) || $_SESSION["username"] == "") echo "Guest"; else echo $_SESSION['username'];?>
+
+<p>Welcome <?php if(!isset($_SESSION["username"])) echo "N?A"; else echo $_SESSION['username'];?>
 </p>
-<?php
-$username=$_SESSION["username"];
-?>
+
 
 <div class="container">
 	<div class="columns">
@@ -48,10 +61,6 @@ $username=$_SESSION["username"];
 
 		<form action="logout.php" method = "post">
 			<input type="submit" class="btn" value="Logout">
-		</form>
-
-		<form action="update.php" method="post">
-			<input type="submit" class="btn" value="Update Info">
 		</form>
 
 		<form action="message.php" method="post">
@@ -65,14 +74,15 @@ $username=$_SESSION["username"];
 		<form action="profile.php" method="post">
 			<input type="submit" class="btn" value="My Profile">
 		</form>
-		
-		<form action="channels\<?php echo $username?>.php" method="post">
-			<input type="submit" class="btn" value="My Channel">
-		</form>
 
 		<form action="playlist.php" method="post">
 			<input type="submit" class="btn" value="My Playlist">
 		</form>
+		
+		<form action="favorites.php" method="post">
+			<input type="submit" class="btn" value="My Favorites">
+		</form>
+		
 
 		<form action="discussion.php" method="post">
 			<input type="submit" class="btn" value="Discussion">
@@ -80,7 +90,7 @@ $username=$_SESSION["username"];
 
 		</div>
 		<div class="column">
-			<a href='media_upload.php'  style="color:#FF9900;">Upload File</a>
+			<a href='media_upload.php' class="btn btn-primary" >Upload File</a>
 			<div id='upload_result'>
 			<?php 
 				if(isset($_REQUEST['result']) && $_REQUEST['result']!=0)
@@ -93,38 +103,113 @@ $username=$_SESSION["username"];
 			</div>
 			<br/><br/>
 			<?php
-
-				$query = "SELECT * from media"; 
-				$result = mysql_query( $query );
-				if (!$result)
-				{
-				die ("Could not query the media table in the database: <br />". mysql_error());
-				}
+    			$query = @mysql_query("SELECT * FROM account");
 			?>
+
+			<form action="browse.php" method="post">
+				<div class="columns">
+					<div class="column col-auto">
+						<?php 
+							echo "<h5> Channel Listings: </h5>";
+						?>
+					</div>
+					<div class="column col-auto">
+						<?php
+							echo "<select class='form-select' style='width: 300px' name='listingdropdown'>";
+							echo "<option></option>";
+							while($row = mysql_fetch_array($query)){
+								echo "<option value='".$row[0]."'>".$row[0]."</option>";
+							}
+							echo "</select>";
+						?>
+					</div>
+					<div class="column col-auto">
+						<input type="submit" class="btn" name="submit" VALUE="Submit" style="width: 200px">
+					</div>
+				</div>
+			</form>
+			<form action="browse.php" method="post">
+				<div class="container">
+					<div class ="columns">
+						<div class="column col-auto">
+							<h5>Search Metube: </h5>
+						</div>
+						<div class="column col-auto">
+							<input type="text" class="form-input" name="search" style="width:300px" placeholder="Keyword Search...";>
+						</div>
+						<div class="column col-auto">
+							<input type="submit" class="btn" VALUE="Search" style="width: 200px">
+						</div>
+					</div>
+				</div>
+			</form>
+			<form action="browse.php" method="post">
+				<div class="container">
+					<div class ="columns">
+						<div class="column col-auto">
+							<h5>Category: </h5>
+						</div>
+						<div class="column col-auto">
+						<select name="category" id="category" class="form-select">
+									<option></option>
+									<option value="Miscellaneous" selected hidden="hidden">Choose here</option>
+									<option value="Auto and Vehicles">Auto and Vehicles</option>
+									<option value="Beauty and Fashion">Beauty and Fashion</option>
+									<option value="Comedy">Comedy</option>
+									<option value="Education">Education</option>
+									<option value="Entertainment">Entertainment</option>
+									<option value="Family Entertainment">Family Entertainment</option>
+									<option value="Film and Animation">Film and Animation</option>
+									<option value="Food">Food</option>
+									<option value="Gaming">Gaming</option>
+									<option value="How-to and Style">How-to and Style</option>
+									<option value="Music">Music</option>
+									<option value="News and Politics">News and Politics</option>
+									<option value="Nonprofits and Activism">Nonprofits and Activism</option>
+									<option value="People and Blogs">People and Blogs</option>
+									<option value="Pets and Animals">Pets and Animals</option>
+									<option value="Science and Technology">Science and Technology</option>
+									<option value="Sports">Sports</option>
+									<option value="Travel and Events">Travel and Events</option>
+									<option value="Miscellaneous">Miscellaneous</option>
+								</select>
+						</div>
+						<div class="column col-auto">
+							<input type="submit" class="btn" VALUE="Search" style="width: 200px">
+						</div>
+					</div>
+				</div>
+
+			</form>
+			<div class='container'>
+				<h3><?php echo $title; ?></h3>
+				<table class="table table-striped" width="100%">
+						<thead>
+							<tr>
+							<th>Media ID</th>
+							<th>Title</th>
+							<th>File Name</th>
+							<th>Description</th>
+							<th>Download</th>
+							</tr>
+						</thead>
+						<tbody>
+							<?php
+									while($result_row = mysql_fetch_row($bresult)){
+										echo "<tr>";
+										echo "<td>".$result_row[0]."</td>";
+										echo "<td>".$result_row[3]."</td>";
+										//echo "<td>".$result_row[1]."</td>";
+										echo "<td><a href='media.php?id=".$result_row[0]."' class='btn' target='_blank'>".$result_row[1]."</a></td>";
+										echo "<td>".$result_row[4]."</td>";
+										echo "<td><a href='".$result_row[2].$result_row[1]."' class='btn' download='".$result_row[2].$result_row[1]."' target='_blank' onclick='javascript:saveDownload(".$result_row[0].");'>Download</a></td>";
+										echo "</tr>";
+									}
+							?>
+					</tbody>
+					</table>
+			</div>
 				
-				<div style="background:#339900;color:#FFFFFF; width:150px;">Uploaded Media</div>
-				<table width="50%" cellpadding="0" cellspacing="0">
-					<?php
-						while ($result_row = mysql_fetch_row($result))
-						{ 
-					?>
-					<tr valign="top">			
-						<td>
-								<?php 
-									echo $result_row[0];
-								?>
-						</td>
-						<td>
-							<a href="media.php?id=<?php echo $result_row[0];?>" target="_blank"><?php echo $result_row[1];?></a> 
-						</td>
-						<td>
-							<a href="<?php echo $result_row[2].$result_row[1];?>" download="<?php echo $result_row[2].$result_row[1];?>" target="_blank" onclick="javascript:saveDownload(<?php echo $result_row[0];?>);">Download</a>
-						</td>
-					</tr>
-					<?php
-						}
-					?>
-				</table>
 		</div>
 	</div>
 </div>
