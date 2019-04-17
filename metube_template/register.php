a48bcd10392ab43e01902f6d7ff2b8c87817654d
ccrawf7 <31708058+ccrawf7@users.noreply.github.com>
2019-04-16 20:11:08 -0400

    updating register.php

--- metube_template/register.php
+++ metube_template/register.php
@@ -1,85 +1,66 @@
+<?php
+  session_start();
+  include_once "function.php";
+?>
 <!DOCTYPE html>
 <html>
-<style>
-input[type=text], select {
-  width: 100%;
-  padding: 12px 20px;
-  margin: 8px 0;
-  display: inline-block;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  box-sizing: border-box;
-}
-input[type=password], select {
-  width: 100%;
-  padding: 12px 20px;
-  margin: 8px 0;
-  display: inline-block;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  box-sizing: border-box;
-}
+<?php
+  
+  if(isset($_SESSION['username'])){
+    header("Location: browse.php");
+  }
 
-input[type=submit] {
-  width: 100%;
-  background-color: #4CAF50;
-  color: white;
-  padding: 14px 20px;
-  margin: 8px 0;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-}
+  if(isset($_POST['submit'])) {
+    if($_POST['lblId'] == "" || $_POST['lpassword'] == "" || $_POST['emailAddress'] == "" || $_POST['clpassword'] == "" || $_POST['firstname'] == "" || $_POST['lastname'] == "" || $_POST['emailAddress'] == "" ) {
+      echo "<script>alert('One or more forms were missing!')</script>";
+      
+    }
+    else if($_POST['lpassword'] != $_POST['clpassword']){
+      echo "<script>alert('Passwords do not match!')</script>";
+    }
+    else {
+      $check = register_ID($_POST['lblId'], $_POST['lpassword'], $_POST['emailAddress'], "0"); // Call functions from function.php
+      if($check == 1){
+        $_SESSION["username"]=$_POST["username"]; //Set the $_SESSION['username']
+        sleep(3);
+        header('Location: login.php');
+      }
+      else{
+        echo "<script>alert('Error Registering!')</script>";
+      }
+    }
+  }
 
-input[type=passwd_result] {
-  width: 100%;
-  background-color: #4CAF50;
-  color: white;
-  padding: 14px 20px;
-  margin: 8px 0;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-}
-
-input[type=submit]:hover {
-  background-color: #45a049;
-}
-
-div {
-  border-radius: 5px;
-  background-color: #f2f2f2;
-  padding: 20px;
-}
-</style>
+?>
+<head>
+<link rel="stylesheet" href="docs/dist/spectre.css">
+</head>
 <body>
 
 <h3>Register Your Account</h3>
 
-<div>
-  <form method='post' action="<?php echo "actionpage.php";?>">
-    <label for="fname">First Name</label>
-    <input type="text" id="fname" name="firstname" placeholder="Your name..">
+<div class="container">
+  <form method='post' action="<?php echo "register.php";?>">
+    <label class="form-label" for="fname">First Name:</label>
+    <input type="text" class="form-input" id="fname" name="firstname" style="width: 300px" placeholder="Your name..">
 
-    <label for="lname">Last Name</label>
-    <input type="text" id="lname" name="lastname" placeholder="Your last name..">
+    <label class="form-label" for="lname">Last Name:</label>
+    <input type="text" class="form-input" style="width: 300px" id="lname" name="lastname" placeholder="Your last name..">
 	
-	<label for="lblId">ID</label>
-    <input type="text" id="lblId" name="lblId" placeholder="Your ID here...">
+	  <label class="form-label" for="lblId">ID:</label>
+    <input type="text" class="form-input" style="width: 300px" id="lblId" name="lblId" placeholder="Your ID here...">
 	
-	<label for="lpassword">Password</label>
-    <input type="password" id="lpassword" name="lpassword" placeholder="Your password here...">
+	  <label class="form-label" for="lpassword">Password:</label>
+    <input class="form-input" style="width: 300px" type="password" id="lpassword" name="lpassword" placeholder="Your password here...">
+    <label class="form-label" for="clpassword">Confirm Password:</label>
+    <input class="form-input" style="width: 300px" type="password" id="clpassword" name="clpassword" placeholder="Confirm password here...">
 	
-    <label for="lemail">Email Address</label>
-    <input type="text" id=lemail name="emailAddress" placeholder="addresss@email.com">
+    <label class="form-label" for="lemail">Email Address</label>
+    <input class="form-input" style="width: 300px" type="text" id=lemail name="emailAddress" placeholder="addresss@email.com">
   
-    <input type="submit" value="submit" name="submit">
+    <input type="submit" class="btn" value="submit" name="submit">
   </form>
 </div>
-<?php
-if(isset($login_error))
-   {  echo "<div id='passwd_result'>".$login_error."</div>";}
-?>
 </body>
 </html>
 
